ec497fb165f96e775d4332adec34686dca9de038
Farrukh Yakubov <fyakubov@users.noreply.github.com>
2022-04-25 20:01:41 +0600

    fixed kovan migration

--- migrations/3_deploy_bankfair_kovan.js
+++ migrations/3_deploy_bankfair_kovan.js
@@ -1,4 +1,4 @@
-var BankFair = artifacts.require("./BankFairPool.sol")
+var BankFair = artifacts.require("./BankFair.sol")
 
 module.exports = async function(deployer) {
   deployer.deploy(BankFair, '0xB347b9f5B56b431B2CF4e1d90a5995f7519ca792', '0x81F42A658D551637fA5EDe6E4Bd75A9688dc2d9C', BigInt(10e18));
