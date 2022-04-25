59be969e0dac6c99c9909cbd18990e4d20b44f29
Farrukh Yakubov <fyakubov@users.noreply.github.com>
2022-04-25 16:11:22 +0600

    update migrations with new contreact name and constructor params

--- migrations/2_deploy_bankfair.js
+++ migrations/2_deploy_bankfair.js
@@ -1,8 +1,8 @@
 var TestToken = artifacts.require("./test/TestToken.sol");
-var BankFairPool = artifacts.require("./BankFairPool.sol")
+var BankFair = artifacts.require("./BankFair.sol")
 
 module.exports = async function(deployer) {
   const accounts = await web3.eth.getAccounts();
   let tokenContract = await deployer.deploy(TestToken, accounts[1], accounts[2], accounts[3], accounts[4]);
-  await deployer.deploy(BankFairPool, tokenContract.address, BigInt(100e18));
+  await deployer.deploy(BankFair, tokenContract.address, accounts[9], BigInt(100e18));
 };
