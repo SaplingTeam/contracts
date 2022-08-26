b491dd5d479230baac3566c899e10694298d3227
Farrukh Yakubov <fyakubov@users.noreply.github.com>
2022-08-26 07:30:04 +0600

    comment update

--- scripts/arguments.lendingpool.polygon.mumbai.js
+++ scripts/arguments.lendingpool.polygon.mumbai.js
@@ -1,5 +1,5 @@
 module.exports = [
-    '0x0000000000000000000000000000000000000000', // placeholder pool address
+    '0x0000000000000000000000000000000000000000', // placeholder pool token address
     '0xD0B3a7f531030dc19651f9F6b31Fc63B47334E86', // capital token
     '0x70f3637e717323b59A4C20977DB92652e584628b', // governance
     '0x99FBBeb892b48e1eb8457d5a6e4991C46f802459', // protocol wallet
