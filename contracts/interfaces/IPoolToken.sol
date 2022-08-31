ff221907816d0fedaf2f05ab3f4a06bcf7d0ab2d
Farrukh Yakubov <fyakubov@users.noreply.github.com>
2022-08-31 20:41:41 +0600

    add license

--- contracts/interfaces/IPoolToken.sol
+++ contracts/interfaces/IPoolToken.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -11,7 +11,7 @@ interface IPoolToken is IERC20 {
 
     /**
      * @notice Mint tokens.
-     * @dev Hook for the lending pool for mining tokens upon pool entry operations. 
+     * @dev Hook for the lending pool for mining tokens upon pool entry operations.
      *      Caller must be the lending pool that owns this token.
      * @param to Address the tokens are minted for
      * @param amount The amount of tokens to mint
@@ -20,7 +20,7 @@ interface IPoolToken is IERC20 {
 
     /**
      * @notice Burn tokens.
-     * @dev Hook for the lending pool for burning tokens upon pool exit or stake loss operations. 
+     * @dev Hook for the lending pool for burning tokens upon pool exit or stake loss operations.
      *      Caller must be the lending pool that owns this token.
      * @param from Address the tokens are burned from
      * @param amount The amount of tokens to burn
