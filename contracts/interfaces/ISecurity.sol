d61b0c53a61669fcddcb34ac5505ef2014cdaa2d
Farrukh Yakubov <fyakubov@users.noreply.github.com>
2022-09-02 12:49:35 +0600

    disable initializers after deployment via factories

---
+++ contracts/interfaces/ISecurity.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+/**
+ * @title Math Context Interface
+ */
+interface ISecurity {
+
+    /**
+     * @dev Disable initializers
+     */
+    function disableIntitializers() external;
+}
