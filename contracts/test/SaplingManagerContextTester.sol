95a4ca70e0326d872857e2bab51a59df1165145c
Farrukh Yakubov <fyakubov@users.noreply.github.com>
2022-12-15 13:12:53 +0000

    test helper contract for SaplingManagerContext

---
+++ contracts/test/SaplingManagerContextTester.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+import "../context/SaplingManagerContext.sol";
+
+/**
+ * @dev Exposes selected internal functions and/or modifiers for direct calling for testing purposes.
+ */
+contract SaplingManagerContextTester is SaplingManagerContext {
+
+    uint256 public value;
+
+    event ValueChanged(uint256 prevValue, uint256 newValue);
+
+    /**
+     * @dev Wrapper for an internal function
+     */
+    function isNonUserAddressWrapper(address party) external view returns (bool) {
+        return isNonUserAddress(party);
+    }
+
+    /**
+     * @dev A state changing function with onlyUser modifier
+     * @param newValue, new 
+     */
+    function someOnlyUserFunction(uint256 newValue) external onlyUser {
+        uint256 prevValue = value;
+        value = newValue;
+
+        emit ValueChanged(prevValue, newValue);
+    }
+}
