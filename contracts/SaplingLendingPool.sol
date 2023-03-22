c16972b5029975904f23b539d641866f37f4c1da
Farrukh Yakubov <fyakubov@users.noreply.github.com>
2023-03-22 22:38:29 +0000

    require nonzero treasury address

--- contracts/SaplingLendingPool.sol
+++ contracts/SaplingLendingPool.sol
@@ -80,6 +80,8 @@ contract SaplingLendingPool is ILendingPool, SaplingPoolContext {
      * @param _treasury New treasury address
      */
     function setTreasury(address _treasury) external onlyRole(SaplingRoles.GOVERNANCE_ROLE) {
+        require(address (_treasury) != address (0), "SaplingLendingPool: invalid treasury address");
+
         address prevTreasury = treasury;
         treasury = _treasury;
         emit TreasurySet(prevTreasury, _treasury);
